26f1af75a8dac17185a3a4ffbfecea2b51f30292
David Siegel <djsiegel@gmail.com>
2019-02-27 15:06:36 -0800

    Support user pages as well

--- tampermonkey/add-retool-button-to-intercom.js
+++ tampermonkey/add-retool-button-to-intercom.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Jump right into Retool :D
 // @author       You
-// @match        https://app.intercom.io/a/apps/*/inbox/*
+// @match        https://app.intercom.io/a/apps/*
 // @grant        none
 // @run-at       document-end
 // @require      http://code.jquery.com/jquery-latest.js
